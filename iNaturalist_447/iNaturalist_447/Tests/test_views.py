46106bc1d2395c54d13a367df68da03b20d6353f
JamesAltF4 <rg14370@umbc.edu>
2020-10-19 18:48:00 -0400

    Add base for testing views and urls

---
+++ iNaturalist_447/iNaturalist_447/Tests/test_views.py
@@ -0,0 +1,17 @@
+"""
+Test to see if each view is correct
+"""
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from ..views import home
+
+
+class TestViews(TestCase):
+
+    # test for the homepage
+    def test_home_view(self):
+        client = Client()
+        response = client.get(reverse(home))
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, '../Templates/home.html')
