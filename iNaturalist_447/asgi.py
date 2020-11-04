33bac021ae0cbafcbdffb2f9152f7bf964bf02d6
ktag0629 <kenta1@umbc.edu>
2020-11-04 15:47:21 -0500

    move iNaturalist_447 to top level and removing redundant top level

--- iNaturalist_447/asgi.py
+++ iNaturalist_447/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for iNaturalist_447 project.
+ASGI config for iNaturalist_447_old project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- iNaturalist_447/settings.py
+++ iNaturalist_447/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = '-ose^#(h%ljy-r2=_kd9k6dci6o-&&pl!m)9#1@4ol*645k%@9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'inaturalistgo.herokuapp.com/']
-
+ALLOWED_HOSTS = ['127.0.0.1', 'inaturalistgo.herokuapp.com']
 
 # Application definition
 
@@ -56,7 +54,7 @@ ROOT_URLCONF = 'iNaturalist_447.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'iNaturalist_447.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
@@ -114,7 +109,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
