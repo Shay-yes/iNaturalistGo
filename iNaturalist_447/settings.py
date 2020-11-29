667b4d6b234a0c20034e1cd4c70f152b258e60c3
ktag0629 <kenta1@umbc.edu>
2020-11-29 14:14:13 -0500

    add species app to project, hooked up settings app config and urls in urls.py

--- iNaturalist_447/settings.py
+++ iNaturalist_447/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'inaturalistgo.herokuapp.com']
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'species.apps.SpeciesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
